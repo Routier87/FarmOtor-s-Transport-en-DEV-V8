9f9bf6145d83d2a79a9bd1524638005dc1798e39
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-03-09 01:23:16 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 const DATA_DIR = path.join(__dirname, 'data');
